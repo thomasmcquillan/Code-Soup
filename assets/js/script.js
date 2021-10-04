db53a7d9a8abf5e35e147892dd88d0648ee6bc75
thomasmcquillan <thomas.mcquillan@gmail.com>
2021-10-04 21:56:13 +0000

    Add js code to display right sleeve image tiles and switch between them

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,3 +29,49 @@ $( "#bodylong" ).click(function() {
     $( "#bodylong" ).toggle(),
     $( "#bodycropped" ).toggle();
 });
+
+$( "#leftsleevenarrowlong" ).click(function() {
+    $( "#leftsleevenarrowlong" ).toggle(),
+    $( "#leftsleevenarrowshort" ).toggle(),
+    $( "#rightsleevenarrowlong" ).toggle(),
+    $( "#rightsleevenarrowshort" ).toggle();
+});
+
+$( "#leftsleevenarrowshort" ).click(function() {
+    $( "#leftsleevenarrowshort" ).toggle(),
+    $( "#leftsleevewidelong" ).toggle();
+});
+
+$( "#leftsleevewidelong" ).click(function() {
+    $( "#leftsleevewidelong" ).toggle(),
+    $( "#leftsleevewideshort" ).toggle();
+});
+
+$( "#leftsleevewideshort" ).click(function() {
+    $( "#leftsleevewideshort" ).toggle(),
+    $( "#leftsleevenarrowlong" ).toggle();
+});
+
+$( "#rightsleevenarrowlong" ).click(function() {
+    $( "#rightsleevenarrowlong" ).toggle(),
+    $( "#rightsleevenarrowshort" ).toggle(),
+    $( "#leftsleevenarrowlong" ).toggle(),
+    $( "#leftsleevenarrowshort" ).toggle();
+});
+
+$( "#rightsleevenarrowshort" ).click(function() {
+    $( "#rightsleevenarrowshort" ).toggle(),
+    $( "#rightsleevewidelong" ).toggle(),
+    $( "#leftsleevenarrowshort" ).toggle(),
+    $( "#leftsleevewidelong" ).toggle();
+});
+
+$( "#rightsleevewidelong" ).click(function() {
+    $( "#rightsleevewidelong" ).toggle(),
+    $( "#rightsleevewideshort" ).toggle();
+});
+
+$( "#rightsleevewideshort" ).click(function() {
+    $( "#rightsleevewideshort" ).toggle(),
+    $( "#rightsleevenarrowlong" ).toggle();
+});
