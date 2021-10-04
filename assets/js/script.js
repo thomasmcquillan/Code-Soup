b64e532ba6bc89bc0fecf450133c84f42d1b7643
thomasmcquillan <thomas.mcquillan@gmail.com>
2021-10-04 20:09:45 +0000

    Add neckline images and link javascript

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $( "#neckcollar" ).click(function() {
+        $( "#neckround.png" ).toggle(),
+        $( "#neckcollar.png" ).toggle();
+    });
+});
+
+console.log("hello");
