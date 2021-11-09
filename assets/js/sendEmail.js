c5eead6183e10205c53d37e357410400de92dd1b
thomasmcquillan <thomas.mcquillan@gmail.com>
2021-11-09 13:42:14 +0000

    Fix emailjs bug and add separate coat instructions form

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,18 +1,48 @@
-const serviceID = 'makeuse';
-const coatTemplateID = 'coat-instructions';
-const TemplateID = 'tshirt-instructions';
+// let serviceID = 'makeuse';
+
+// const coatTemplateID = 'coat-instructions';
+let TemplateID = 'tshirt-instructions';
+// const from_name = document.getElementById('from_name');
+const to_name = "Make/Use";
 const btn = document.getElementById('form-button');
+const reply_to = "thomas.mcquillan@gmail.com"
+
+
+const contactFormTshirt = document.getElementById('contact-form-tshirt');
+const contactFormCoat = document.getElementById('contact-form-coat');
 
 window.onload = function() {
-    document.getElementById('contact-form').addEventListener('submit', function(event) {
-        event.preventDefault();
+    document.getElementById('contact-form-tshirt').addEventListener('submit', function(event) {
+      event.preventDefault();
+    emailjs.send("makeuse", "tshirt-instructions", {
+        "from_name": contactFormTshirt.from_name.value,
+        "from_email": contactFormTshirt.from_email.value
+    })
+    .then (    
+        function(response)
+            {console.log('SUCCESS!');
+        }, function(error) {
+            console.log('FAILED...', error);
+        }
+    );
+    return false;
+});
+}
 
-        btn.value = "Sending..";
-        emailjs.sendForm('makeuse', 'tshirt-instructions', this)
-            .then(function() {
-                console.log('SUCCESS!');
-            }, function(error) {
-                console.log('FAILED...', error);
-            });
-    });
+window.onload = function() {
+    document.getElementById('contact-form-coat').addEventListener('submit', function(event) {
+      event.preventDefault();
+    emailjs.send("makeuse", "coat-instructions", {
+        "from_name": contactFormCoat.from_name.value,
+        "from_email": contactFormCoat.from_email.value
+    })
+    .then (    
+        function(response)
+            {console.log('SUCCESS!');
+        }, function(error) {
+            console.log('FAILED...', error);
+        }
+    );
+    return false;
+});
 }
